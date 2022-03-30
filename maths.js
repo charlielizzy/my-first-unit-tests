a8278710b76900fd5fbfe8534503eef4f183ce14
Tom Groombridge <tom.groombridge@zopa.com>
2022-03-30 11:00:14 +0100

    inital commit

---
+++ maths.js
@@ -0,0 +1,19 @@
+export const add = (a, b) => {
+  return a + b
+}
+
+export const subtract = (a, b) => {
+  return a - b
+}
+
+export const multiple = (a, b) => {
+  return a * b
+}
+
+export const divide = (a, b) => {
+  return a / b
+}
+
+export const remainder = (a, b) => {
+  return a % b
+}
---
+++ maths.test.js
@@ -0,0 +1,5 @@
+import { add } from './maths.js';
+
+test('add should return 4', () => {
+  expect(add(3, 1)).toEqual(4);
+})
